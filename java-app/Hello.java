a7d4fd492e553783ec8eabbd0b008b618aae4cdc
Ubuntu <ubuntu@ip-172-31-4-170.eu-north-1.compute.internal>
2024-06-10 15:12:39 +0000

    Initial commit

---
+++ java-app/Hello.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Hello
+{
+    public static void main(String[] args)
+    {
+              System.out.println("Hello Dosto");
+              Date currentDate= new Date();
+     	      System.out.println("today's date is: "+ currentDate);
+    }
+} 
